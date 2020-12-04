ad2537e87185152ff206c89ff77060f970c2570d
stormich <stormich8@gmail.com>
2020-12-03 20:17:08 -0800

    Day 2

--- 2020/CSharpAdvent/Day_2.cs
+++ 2020/CSharpAdvent/Day_2.cs
@@ -11,7 +11,32 @@ namespace CSharpAdvent
         // How many passwords are valid according to their policies?
         public int ValidPasswordsPartOne(List<string> lines)
         {
-            return -1;
+            int totalCount = 0;
+            for(int i = 0; i < lines.Count; i++)
+            {
+                char[] delimiterChars = { ':', '-', ' '};
+                string[] splitArr = lines[i].Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+                int min = int.Parse(splitArr[0]);
+                int max = int.Parse(splitArr[1]);
+                char x = char.Parse(splitArr[2]);
+                string word = splitArr[3];
+                
+                int count = 0;
+
+                for(int j = 0; j < word.Length; j++)
+                {
+                    if(word[j] == x)
+                    {
+                        count++;
+                    }
+                }
+
+                if(count >= min && count <= max)
+                {
+                    totalCount++;
+                }
+            }
+            return totalCount;
         }
 
         // Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. 
@@ -22,7 +47,34 @@ namespace CSharpAdvent
         // 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
         public int ValidPasswordsPartTwo(List<string> lines)
         {
-            return -1;
+            int totalCount = 0;
+            for(int i = 0; i < lines.Count; i++)
+            {
+                char[] delimiterChars = { ':', '-', ' '};
+                string[] splitArr = lines[i].Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+                int pos1 = int.Parse(splitArr[0]);
+                int pos2 = int.Parse(splitArr[1]);
+                char x = char.Parse(splitArr[2]);
+                string word = splitArr[3];
+                
+                bool match = false;
+                if(pos1 <= word.Length && word[pos1-1] == x)
+                {
+                    totalCount++;
+                    match = true;
+                }
+                if(pos2 <= word.Length && word[pos2-1] == x)
+                {
+                    if(!match)
+                    {
+                        totalCount++;
+                    }else
+                    {
+                        totalCount--;
+                    }
+                }
+            }
+            return totalCount;
         }
     }
 }
--- 2020/CSharpAdvent/Program.cs
+++ 2020/CSharpAdvent/Program.cs
@@ -8,33 +8,37 @@ namespace CSharpAdvent
         static void Main(string[] args)
         {
             FileReader oFileReader = new FileReader();
+
+            System.Console.WriteLine();
             // Day-1 - Expense report
             Day_1 oDayOne = new Day_1();
-
+            System.Console.WriteLine("----- DAY ONE -----");
             // Get input from file and add it to list
             List<string> linesPartOne = oFileReader.getLines("day-1-input.txt");
 
             //Day-1-Part 1: Find the two entries that sum to 2020 and then multiply those two numbers together.
             int resultPartOne = oDayOne.MultiplySumOfTwoNumbers(linesPartOne);
-            System.Console.WriteLine(resultPartOne);
+            System.Console.WriteLine("Result part1: " + resultPartOne);
+            System.Console.WriteLine();
             //Day-1-Part 3: Find the three entries that sum to 2020 and then multiply those two numbers together.
             int resultPartTwo = oDayOne.MultiplySumOfThreeNumbersOf2020(linesPartOne);
-            System.Console.WriteLine(resultPartTwo);
+            System.Console.WriteLine("Result part1: " + resultPartTwo);
 
             // ------------------------------------------------------------------------------------------------------------------------
-
+            System.Console.WriteLine();
             // Day-2: Password Philosophy
             Day_2 oDayTwo = new Day_2();
+            System.Console.WriteLine("----- DAY TWO -----");
             // Each line gives the password policy and then the password. 
             // Get input from file and add it to list
             List<string> linesPartTwo = oFileReader.getLines("day-2-input.txt");
 
             // Day-2-Part-1
             // The password policy indicates the lowest and highest number of times a given letter must appear for the password to be valid. 
-            // For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+            // For example, 1-3 'a' means that the password must contain 'a' at least 1 time and at most 3 times.
             // How many passwords are valid according to their policies?
             int resultDayTwoPartOne = oDayTwo.ValidPasswordsPartOne(linesPartTwo);
-            System.Console.WriteLine(resultDayTwoPartOne);
+            System.Console.WriteLine("Valid passwords for part1: " + resultDayTwoPartOne);
 
             // Day-2-Part-2
             // Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. 
@@ -44,7 +48,7 @@ namespace CSharpAdvent
             // 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
             // 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
             int resultDayTwoPartTwo = oDayTwo.ValidPasswordsPartTwo(linesPartTwo);
-            System.Console.WriteLine(resultDayTwoPartTwo);
+            System.Console.WriteLine("Valid passwords for part2: " + resultDayTwoPartTwo);
         }
     }
 }
